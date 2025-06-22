c3f9fd69fa74eab29cc7bd5b258daf99952ba78d
Tharindu Sandaruwan <tharindusandaruwan40@gmail.com>
2025-06-22 23:40:06 +0800

    [#issue-3166052940][1.0.0]-added db config to cater future changes as well

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,9 +2,16 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "./src/config/config.js"
+import connectDB from "./src/config/db.js";
 
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`🚀💨 Server running on port ${PORT}`));
+})
